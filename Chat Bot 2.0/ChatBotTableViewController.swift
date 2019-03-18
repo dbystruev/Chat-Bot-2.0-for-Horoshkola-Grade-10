9fe573f5c37e3e127f96feb2c382ffd3803f5b86
Denis Bystruev <dbystruev@me.com>
2019-03-18 10:46:30 +0300

    Added Table View Controller

---
+++ Chat Bot 2.0/ChatBotTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ChatBotTableViewController.swift
+//  Chat Bot 2.0
+//
+//  Created by Denis Bystruev on 25/02/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+class ChatBotTableViewController: UITableViewController {
+
+}
